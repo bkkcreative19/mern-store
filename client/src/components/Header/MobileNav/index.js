1456bd1976457dee646f99117a38651b1be2fb7b
bkkcreative19 <bkkcreative16@gmail.com>
2022-04-27 12:53:17 -0500

    started responsive nav

---
+++ client/src/components/Header/MobileNav/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./MobileNav.scss";
+
+export const MobileNav = () => {
+  return <nav className="mobile-nav">hi</nav>;
+};
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import { AiOutlineSearch, AiOutlineUser } from "react-icons/ai";
 import { RiShoppingBag2Line } from "react-icons/ri";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
 
 import "./Header.scss";
+import { MobileNav } from "./MobileNav";
 
 export const Header = () => {
   const cart = useSelector((state) => state.cart);
+  const [isOpen, setIsOpen] = useState(false);
 
   const { cartItems } = cart;
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "scroll";
+    }
+  }, [isOpen]);
 
   return (
     <header className="header">
       <nav className="header__nav container">
-        <GiHamburgerMenu className="header__nav-hamburger" />
+        {isOpen && <MobileNav />}
+        {isOpen ? (
+          <GrClose
+            className="header__nav-hamburger"
+            onClick={() => setIsOpen(false)}
+          />
+        ) : (
+          <GiHamburgerMenu
+            onClick={() => setIsOpen(true)}
+            className="header__nav-hamburger"
+          />
+        )}
+
         <Link to="/">
           <h2 className="header__nav-logo">CUE</h2>
         </Link>
