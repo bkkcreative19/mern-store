e301e3c4324109d52db7f2c5cc9ba0bf623eedb2
bkkcreative19 <bkkcreative16@gmail.com>
2022-05-04 18:53:12 -0500

    added stripe

--- client/src/components/Information/index.js
+++ client/src/components/Information/index.js
@@ -1,33 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 import { useNavigate, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./Information.scss";
+import { CheckoutForm } from "../CheckoutForm";
+import { apiUrl } from "../../utils/apiURL";
+
+const stripePromise = loadStripe(process.env.REACT_APP_PK_KEY);
 
 export const Information = () => {
+  const [clientSecret, setClientSecret] = useState("");
   const navigate = useNavigate();
+  const cart = useSelector((state) => state.cart);
+  console.log(cart);
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch(`${apiUrl}/order`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(cart.cartItems),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
   return (
-    <section className="information">
-      <h2>Shipping address</h2>
-      <form className="info-form">
-        <div className="name">
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
-        </div>
-        <input placeholder="Address" type="text" className="address" />
-        <input
-          placeholder="Apartment (optional)"
-          type="text"
-          className="apartment"
-        />
-        <input placeholder="City" type="text" className="city" />
-        <div className="buttons">
-          <Link to="/checkout/shipping">
-            <button className="continue-shopping">Continue Shopping</button>
-          </Link>
-          <Link to="/cart">
-            <button className="return-to-cart">Return to cart</button>
-          </Link>
-        </div>
-      </form>
-    </section>
+    <>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </>
+    // <section className="information">
+    //   <h2>Shipping address</h2>
+    //   <form className="info-form">
+    //     <div className="name">
+    //       <input type="text" placeholder="First Name" />
+    //       <input type="text" placeholder="Last Name" />
+    //     </div>
+    //     <input placeholder="Address" type="text" className="address" />
+    //     <input
+    //       placeholder="Apartment (optional)"
+    //       type="text"
+    //       className="apartment"
+    //     />
+    //     <input placeholder="City" type="text" className="city" />
+    //     <div className="buttons">
+    //       <Link to="/checkout/shipping">
+    //         <button className="continue-shopping">Continue Shopping</button>
+    //       </Link>
+    //       <Link to="/cart">
+    //         <button className="return-to-cart">Return to cart</button>
+    //       </Link>
+    //     </div>
+    //   </form>
+    // </section>
   );
 };
--- controller/orderController.js
+++ controller/orderController.js
@@ -1,16 +1,38 @@
 const asyncHandler = require("../middleware/async");
+const stripe = require("stripe")(process.env.SK_key);
 const Order = require("../models/orderModel");
 
 const createOrder = asyncHandler(async (req, res, next) => {
-  console.log(req.body.body);
-  const newOrder = await Order.create({
-    ...req.body.body,
-    // userId: req.user._id,
+  // console.log(req.body.body);
+  // const newOrder = await Order.create({
+  //   ...req.body.body,
+  //   // userId: req.user._id,
+  // });
+  const { items } = req.body;
+  console.log(items);
+
+  const calculateOrderAmount = (items) => {
+    return req.body
+      .reduce((acc, item) => acc + item.qty * item.price, 0)
+      .toFixed(2);
+  };
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: Number(calculateOrderAmount(items)),
+    currency: "eur",
+    automatic_payment_methods: {
+      enabled: true,
+    },
   });
 
-  res
-    .status(201)
-    .send({ status: "success", message: "New Order Created", data: newOrder });
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+
+  // res
+  //   .status(201)
+  //   .send({ status: "success", message: "New Order Created", data: newOrder });
 });
 
 module.exports = {
